6936e0047d52ec625c99e37ca1cd6cd9faa6e631
jiahao <felix.jiahao.feng@outlook.com>
2022-04-07 19:52:47 +0800

    TermCurve: add test for term ir curve

--- test/TestCurve.py
+++ test/TestCurve.py
@@ -1,4 +1,5 @@
 from curve.const_curve import ConstCurve
+from curve.term_curve import TermCurve
 from datetime import datetime
 import math
 import unittest
@@ -10,10 +11,22 @@ class TestCurve(unittest.TestCase):
         self.const_rate = 0.025
         self.const_curve = ConstCurve(valuation_date=valuation_date,
                                       rate=self.const_rate)
+        self.term_curve = TermCurve(valuation_date=valuation_date,
+                                    terms=[1, 2, 3],
+                                    rates=[0.02, 0.025, 0.028])
 
     def test_const_curve(self):
         terms = [0, 0.5, 1, 3]
         for term in terms:
             self.assertEqual(self.const_rate, self.const_curve.rate(tau=term))
         for term in terms:
-            self.assertEqual(math.exp(-self.const_rate * term), self.const_curve.discount(term))
+            self.assertEqual(math.exp(-self.const_rate * term), self.const_curve.discount(tau=term))
+
+    def test_term_curve(self):
+        rates = [0.02, 0.02, 0.021, 0.025, 0.0274, 0.028, 0.028]
+        terms = [0.5, 1, 1.2, 2, 2.8, 3, 3.5]
+        for i in range(len(terms)):
+            term = terms[i]
+            self.assertEqual(self.term_curve.rate(tau=term), rates[i])
+        for i in range(len(terms)):
+            self.assertEqual(math.exp(-rates[i] * terms[i]), self.term_curve.discount(tau=terms[i]))
