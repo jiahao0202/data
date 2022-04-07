86501e6b0dbefac3a42f94446361af6d1723aa72
jiahao <felix.jiahao.feng@outlook.com>
2022-04-07 19:52:23 +0800

    LinearInterp: change the method for looking up to private variables

--- numerics/interp/linear_interp.py
+++ numerics/interp/linear_interp.py
@@ -13,9 +13,9 @@ class LinearInterp1d(BaseInterp1d, ABC):
         self.fill_value = fill_value
 
         if self.mode == "flat":
-            if self.fill_value is None:
-                self.left_fill_val = self.__x[0]
-                self.right_fill_val = self.__y[0]
+            if self.fill_value is None or len(self.fill_value) == 0:
+                self.left_fill_val = self._y_vals[0]
+                self.right_fill_val = self._y_vals[-1]
             elif len(self.fill_value) == 1:
                 self.left_fill_val = self.fill_value[0]
                 self.right_fill_val = self.fill_value[0]
@@ -25,14 +25,14 @@ class LinearInterp1d(BaseInterp1d, ABC):
             raise NotImplemented
 
     def interp(self, x_) -> float:
-        if x_ <= self.__x[0]:
+        if x_ <= self._x_vals[0]:
             return self.left_fill_val
-        elif x_ >= self.__x[-1]:
+        elif x_ >= self._x_vals[-1]:
             return self.right_fill_val
         else:
-            index = np.searchsorted(self.__x, x_)
-            y_low = self.__y[index - 1]
-            y_high = self.__y[index]
-            x_low = self.__x[index - 1]
-            x_high = self.__x[index]
+            index = np.searchsorted(self._x_vals, x_)
+            y_low = self._y_vals[index - 1]
+            y_high = self._y_vals[index]
+            x_low = self._x_vals[index - 1]
+            x_high = self._x_vals[index]
             return y_low + (y_high - y_low) / (x_high - x_low) * (x_ - x_low)
