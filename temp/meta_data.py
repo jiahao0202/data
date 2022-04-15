c5c3ca2fce87b8abfaa3eeb03be438b90bf3ac86
jiahao0202 <103086075+jiahao0202@users.noreply.github.com>
2022-04-15 14:31:33 +0800

    add temp branch

---
+++ temp/meta_data.py
@@ -0,0 +1,41 @@
+import pickle
+from datetime import datetime
+from patterns.singleton import Singleton
+
+
+def decode_pickle(file):
+    with open(file, 'rb') as f:
+        res = pickle.load(f)
+    res = pickle.loads(res)
+    return res
+
+
+class MetaData(metaclass=Singleton):
+    trading_cal = decode_pickle("./trade_cal.pickle")
+    exp_map_3mo = decode_pickle('./exp_3m.pickle')
+    exp_map_6mo = decode_pickle('./exp_6m.pickle')
+    exp_map_12mo = decode_pickle('./exp_12m.pickle')
+
+    @staticmethod
+    def year_fraction_trading(valuation_date: datetime, expiration_date: datetime):
+        valuation_date = valuation_date.strftime("%Y-%m-%d")
+        expiration_date = expiration_date.strftime("%Y-%m-%d")
+        date_diff = MetaData.trading_cal.index(expiration_date) - MetaData.trading_cal.index(valuation_date)
+        return date_diff / 244.
+
+    @staticmethod
+    def year_fraction(valuation_date: datetime, expiration_date: datetime):
+        date_diff = (expiration_date.timestamp() - valuation_date.timestamp()) / (3600 * 24)
+        return date_diff / 365.
+
+    @staticmethod
+    def get_expiration_date_3mo(valuation_date) -> datetime:
+        return datetime.strptime(MetaData.exp_map_3mo[valuation_date], "%Y-%m-%d")
+
+    @staticmethod
+    def get_expiration_date_6mo(valuation_date) -> datetime:
+        return datetime.strptime(MetaData.exp_map_6mo[valuation_date], "%Y-%m-%d")
+
+    @staticmethod
+    def get_expiration_date_12mo(valuation_date) -> datetime:
+        return datetime.strptime(MetaData.exp_map_12mo[valuation_date], "%Y-%m-%d")
---
+++ temp/vanilla_option_data_processor.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from market_data.surface.term_vol_surface import TermVolSurface
+from temp.meta_data import MetaData
+
+vol_schemes = {
+    'flat_3m': '3m',
+    'flat_6m': '6m',
+    'flat_12m': '12m',
+    'term_vol_3': ['3m', '6m', '12m'],
+    'term_vol_12': ['1m', '2m', '3m', '4m', '5m', '6m', '7m', '8m', '9m', '10m', '11m', '12m']
+}
+
+vol_terms = {
+    'flat_3m': 63,
+    'flat_6m': 126,
+    'flat_12m': 252,
+    'term_vol_3': [63, 126, 252],
+    'term_vol_12': [21, 42, 63, 84, 105, 126, 147, 168, 189, 210, 231, 252]
+}
+
+
+def process_individual_stock(stock: pd.DataFrame, vol_scheme: str):
+    stock['r'] = 0.025
+    stock['q'] = 0.
+    expiration_date = max(stock.index)
+    stock['tau'] = stock.apply(lambda x: MetaData.year_fraction_trading(x.name, expiration_date), axis=1)
+    if vol_scheme.startswith("flat"):
+        stock['vol'] = stock[vol_scheme]
+    else:
+        terms = [x / 252 for x in vol_terms[vol_scheme]]
+        stock['vol_surface'] = stock.apply(lambda x:
+                                           TermVolSurface(valuation_date=x.name,
+                                                          terms=terms,
+                                                          term_vols=list(x.loc[vol_schemes[vol_scheme]])),
+                                           axis=1)
+        stock['vol'] = stock.apply(lambda x: x['vol_surface'].vol(tau=x['tau'], strike=1.))
+    return stock[['close', 'tau', 'r', 'q', 'vol']]
