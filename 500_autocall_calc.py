8c9f711a70eb109eb90a49b2abde5ba951fb64b1
jiahaofeng <jfengan@connet.ust.hk>
2022-04-18 20:16:05 +0800

    500 autocall pricing

--- 500_autocall_calc.py
+++ 500_autocall_calc.py
@@ -4,6 +4,7 @@ import numpy as np
 from market_data.surface.base_surface import BaseVolSurface
 from market_data.surface.const_vol_surface import ConstVolSurface
 from market_data.surface.term_vol_surface import TermVolSurface
+from numerics.solver import Solver
 from temp.meta_data import *
 from temp.vanilla_option_data_processor import vol_schemes, vol_terms
 
@@ -18,13 +19,17 @@ def calc_step_vol(tau, vol_surface: BaseVolSurface):
 
 
 if __name__ == "__main__":
-    vol_scheme = 'flat_3m'
+    parser = optparse.OptionParser()
+    parser.add_option('-v', dest='vol_scheme', type='string')
+    (options, args) = parser.parse_args()
+    vol_scheme = options.vol_scheme
     test_dict = decode_pickle("./temp/autocall_meta_data.pickle")
+    coupon_result = {}
     for key, value in test_dict.items():
         print(key)
         frame = pd.read_pickle(f'./000905/{key}.pickle')
         if vol_scheme.startswith('flat'):
-            frame['vol_surface'] = frame.apply(lambda x: ConstVolSurface(x.name, vol_schemes[vol_scheme]), axis=1)
+            frame['vol_surface'] = frame.apply(lambda x: ConstVolSurface(x.name, x[vol_schemes[vol_scheme]]), axis=1)
         else:
             frame['vol_surface'] = frame.apply(lambda x: TermVolSurface(x.name,
                                                                         [x / 244 for x in vol_terms[vol_scheme]],
@@ -33,3 +38,17 @@ if __name__ == "__main__":
         frame['tau'] = frame.apply(lambda x: MetaData.year_fraction_trading(x.name, exp_dt), axis=1)
         frame['vols'] = frame.apply(lambda x: calc_step_vol(x['tau'], x['vol_surface']), axis=1)
 
+        coupon_rate = Solver.autocall_coupon_solver(spot=frame['close'].values[0],
+                                                    r=0.025,
+                                                    q=0.,
+                                                    vol=frame['vols'].values[0],
+                                                    tau=frame['tau'].values[0],
+                                                    ko_price=value['ko_price'],
+                                                    ki_price=value['ki_price'],
+                                                    ko_list=np.array(value['ko_list']),
+                                                    natural_day_list=np.array(value['nat_ko_list']),
+                                                    num_paths=100000)
+        coupon_result[key] = coupon_rate
+    with open(f"./coupon/{vol_scheme}.pickle", 'wb') as f:
+        data = pickle.dumps(coupon_result)
+        pickle.dump(data, f)
--- temp/autocall_evaluate.py
+++ temp/autocall_evaluate.py
@@ -15,7 +15,8 @@ def autocall_mc_pricer(normal_dist, spot, r, q, vol, tau, dt, ko_list, num_paths
     paths = np.zeros((num_paths, step_size + 1))
     paths[:, 0] = spot
     for i in range(1, step_size + 1):
-        paths[:, i] = paths[:, i - 1] * np.exp((r - q - .5 * vol ** 2) * dt + vol * np.sqrt(dt) * normal_dist[:, i - 1])
+        paths[:, i] = paths[:, i - 1] * np.exp((r - q - .5 * vol[i-1] ** 2) *
+                                               dt + vol[i-1] * np.sqrt(dt) * normal_dist[:, i - 1])
     payoffs = np.zeros(num_paths)
     flag_ko = np.zeros(num_paths)
     flag_ki = np.zeros(num_paths)
@@ -26,13 +27,12 @@ def autocall_mc_pricer(normal_dist, spot, r, q, vol, tau, dt, ko_list, num_paths
                 nat_day = natural_day_list[j]
                 payoffs[i] = (notional * coupon_rate * nat_day / 365) * np.exp(-r * nat_day / 365)
                 flag_ko[i] = 1
-                break
 
     for i in range(num_paths):
         if flag_ko[i] == 0:
             if np.min(paths[i]) < ki_price:
                 payoffs[i] = notional * np.minimum(paths[i, -1] / paths[i, 0] - 1, 0) * \
-                             np.exp(-r * natural_day_list[-1])
+                             np.exp(-r * natural_day_list[-1] / 365)
                 flag_ki[i] = 1
     payoffs = np.where(flag_ko + flag_ki == 0,
                        notional * coupon_rate * np.exp(-r * natural_day_list[-1] / 365), payoffs)
