72dbe89bc6e78c5021f83651b5df176e1629c299
jiahao0202 <103086075+jiahao0202@users.noreply.github.com>
2022-04-18 18:04:26 +0800

    Autocall solver

--- numerics/solver.py
+++ numerics/solver.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numerics.bs_pricer.vanilla_pricer import VanillaOptionPricer
 from scipy.optimize import brentq
+from temp.autocall_evaluate import autocall_mc_pricer, rng
 from utils.product_util import OptionTypeEnum
 
 
@@ -28,3 +29,31 @@ class Solver:
         else:
             solved_vol = brentq(func, lower_bdd, upper_bdd, xtol=precision, maxiter=max_iter)
             return solved_vol
+
+    @staticmethod
+    def autocall_coupon_solver(spot,
+                               r,
+                               q,
+                               vol,
+                               tau,
+                               ko_price,
+                               ki_price,
+                               ko_list,
+                               natural_day_list,
+                               num_paths=50000,
+                               dt=1. / 244.,
+                               precision=1e-6,
+                               max_iter=100
+                               ):
+        lower_bdd = 0.
+        upper_bdd = 1.
+
+        def func(coupon_rate_to_solve):
+            normal_dist_ = rng(num_paths, tau, dt)
+            return autocall_mc_pricer(normal_dist=normal_dist_, spot=spot, r=r, q=q, vol=vol, tau=tau,
+                                      dt=dt, ko_list=ko_list, num_paths=num_paths, ko_price=ko_price,
+                                      ki_price=ki_price, coupon_rate=coupon_rate_to_solve,
+                                      natural_day_list=natural_day_list)
+
+        solved_coupon = brentq(func, lower_bdd, upper_bdd, xtol=precision, maxiter=max_iter)
+        return solved_coupon
